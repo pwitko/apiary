130e5edfd2ae76ed222f172753e50c300cbecd5d
pwitko <36098936+pwitko@users.noreply.github.com>
2021-04-29 12:32:26 +0200

    Initial commit

---
+++ src/main/java/ovh/nowomlyny/apiary/ApiaryApplication.java
@@ -0,0 +1,13 @@
+package ovh.nowomlyny.apiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ovh/nowomlyny/apiary/ApiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package ovh.nowomlyny.apiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
