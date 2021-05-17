4d4e663edb0872a7d60099456b7fa4def59b2458
pwitko <36098936+pwitko@users.noreply.github.com>
2021-05-17 12:56:21 +0200

    Added Methods in Apiary

--- src/main/java/ovh/nowomlyny/apiary/model/Apiary.java
+++ src/main/java/ovh/nowomlyny/apiary/model/Apiary.java
@@ -3,4 +3,21 @@ package ovh.nowomlyny.apiary.model;
 import java.util.ArrayList;
 
 public class Apiary {
+    private ArrayList<Hive> hivesList = new ArrayList<>();
+
+    public ArrayList<Hive> getHivesList() {
+        return hivesList;
+    }
+
+    public void setHivesList(ArrayList<Hive> hivesList) {
+        this.hivesList = hivesList;
+    }
+
+    public void addHiveToList(Hive hive){
+        hivesList.add(hive);
+    }
+
+    public void removeHiveFromList(Hive hive){
+        hivesList.remove(hive);
+    }
 }
